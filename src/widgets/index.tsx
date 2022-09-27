4cb569f7d0c564fcf9dda4248766640cc100db28
jamesb <experimentallearning0@gmail.com>
2022-09-27 10:23:58 +0100

    Doc comment for adding settings for advanced themes

--- src/widgets/index.tsx
+++ src/widgets/index.tsx
@@ -1,8 +1,24 @@
-import { declareIndexPlugin, ReactRNPlugin, WidgetLocation } from '@remnote/plugin-sdk';
+import { declareIndexPlugin, ReactRNPlugin } from '@remnote/plugin-sdk';
 import '../style.css';
 import '../App.css';
 
 async function onActivate(plugin: ReactRNPlugin) {
+  // For advanced themes you can offer settings to
+  // the user to control certain styles, eg: 
+  //
+  // await plugin.settings.registerBooleanSetting({
+  //   id: "change-theme",
+  //   title: "Change the Theme",
+  //   description: "Toggling this setting will change the theme by registering / unregistering some custom CSS.",
+  //   defaultValue: false,
+  // })
+
+  // plugin.track(async (reactivePlugin) => {
+  //   const shouldChangeTheme = await reactivePlugin.settings.getSetting("change-theme");
+  //   if (shouldChangeTheme) {
+  //     await plugin.app.registerCSS('css-id', `...`)
+  //   }
+  // })
 }
 
 async function onDeactivate(_: ReactRNPlugin) {}
